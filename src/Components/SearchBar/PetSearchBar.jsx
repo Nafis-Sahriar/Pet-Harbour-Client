2f50f84959e0fb5d6465799e79afeda600a1282e
Nafis-Sahriar <nafis.apid@gmail.com>
2026-05-20 15:19:42 +0600

    Changed the fetch link with query parameter and the searchTerm.

---
+++ src/Components/SearchBar/PetSearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PetSearchBar = () => {
+
+
+
+
+    // I have to write the searching fucntionality here and design a searchbar , then implemetn the saerching.
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PetSearchBar;
--- src/lib/pets/data.js
+++ src/lib/pets/data.js
@@ -1,22 +1,18 @@
-export const fetchPets = async () => {
+export const fetchPets = async (searchTerm ="") => 
+{
     
-   const res = await fetch(
-      "http://localhost:5000/allpets",
+   const res = await fetch(`http://localhost:5000/allpets?search=${searchTerm}`,
       {
          cache:"no-store"
       }
    );
 
-   if(!res.ok){
-
-      throw new Error(
-         "Failed to fetch pets"
-      );
+   if(!res.ok)
+   {
+      throw new Error("Failed to fetch pets");
 
    }
-
    const data = await res.json();
-
    return data || [];
 
 }
